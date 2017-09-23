2db9a4fcd6d991a91bd2b272caf9e07643852e7b
Ben <Ben@DESKTOP-L61HVFP>
2017-09-23 09:23:55 +0800

    20170923

---
+++ GeneralService.Comunicate.Message/Models/StateOject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GeneralService.Comunicate.Message.Models
+{
+    public class StateOject
+    {
+        public Socket workSocket = null;
+        public const int BufferSize = 256;
+        public byte[] recieveBuffer = new byte[BufferSize];
+        public StringBuilder recieveSB = new StringBuilder();
+    }
+}
---
+++ GeneralService.Comunicate.Message/TextMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GeneralService.Comunicate.Message
+{
+    using Models;
+    class TextMessage
+    {
+        public string ismgIP { get; set; }
+        public int port { get; set; }
+
+        private ManualResetEvent connectDone = new ManualResetEvent(false);
+        private ManualResetEvent sendDone = new ManualResetEvent(false);
+        private ManualResetEvent receiveDone = new ManualResetEvent(false);
+        // The response from the remote device.     
+        private String response = String.Empty;
+
+        private void StartClient()
+        {
+            try
+            {
+                IPAddress ismgIpAddress = IPAddress.Parse(ismgIP);
+                IPEndPoint ismgEndPoint = new IPEndPoint(ismgIpAddress, port);
+
+                Socket ispClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //ispClient.BeginConnect(ismgEndPoint,new AsyncCallback())
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
